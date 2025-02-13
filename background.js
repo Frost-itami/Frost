12f2f155856b528c079b1d47741548607e1cabf2
Frost <abderahimfcb45@gmail.com>
2025-02-13 17:56:04 +0100

    ADD some shit

--- background.js
+++ background.js
@@ -1,41 +1,11 @@
-const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-const ctx = canvas.getContext("2d");
+const bg = document.querySelector('.background-animation');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const stars = [];
-for (let i = 0; i < 100; i++) { // Increased star count
-    stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 2.5,
-        speed: Math.random() * 1 + 0.3,
-    });
+let x = 0, y = 0;
+function animateBackground() {
+    x += 0.1;
+    y += 0.1;
+    bg.style.backgroundPosition = `${x}px ${y}px`;
+    requestAnimationFrame(animateBackground);
 }
 
-// Parallax Effect
-document.addEventListener("mousemove", (e) => {
-    let moveX = (e.clientX / canvas.width) * 2 - 1;
-    let moveY = (e.clientY / canvas.height) * 2 - 1;
-
-    stars.forEach((star) => {
-        star.x += moveX * 0.3;
-        star.y += moveY * 0.3;
-    });
-});
-
-function animateStars() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "rgba(0, 255, 255, 0.8)";
-    stars.forEach((star) => {
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-        ctx.fill();
-        star.y += star.speed;
-        if (star.y > canvas.height) star.y = 0;
-    });
-    requestAnimationFrame(animateStars);
-}
-animateStars();
+animateBackground();
